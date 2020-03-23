da325f29bff34f1b489901f9a7144873d91d2fc1
MOBIUS <hd2dj07@gmail.com>
2020-03-23 19:26:31 +0900

    radio 부트오류 수정

--- backend/radio/settings.py
+++ backend/radio/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'radio.urls'
 
 TEMPLATES = [
     {
@@ -77,7 +77,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'radio.wsgi.application'
 
 
 # Database
--- backend/radio/urls.py
+++ backend/radio/urls.py
@@ -35,7 +35,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]+ schema_url_patterns
+] + schema_url_patterns
 
 urlpatterns += [
     re_path(r'^docs(?P<format>\.json|\.yaml)/$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
