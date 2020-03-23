5b4039fba72fddfb006b706321b62c9ef33579da
MOBIUS <hd2dj07@gmail.com>
2020-03-23 19:24:38 +0900

    radio로 모듈이름 변경

--- backend/radio/settings.py
+++ backend/radio/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend',
+
+    'radio',
 
     'rest_framework',
     'drf_yasg',
