ed39ed69d3953bf4359a9093186dd449cb9d78a7
MOBIUS <hd2dj07@gmail.com>
2020-03-25 16:19:15 +0900

    패키징 준비

---
+++ backend/django_utils/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="django_utils-whiteblue3", # Replace with your own username
+    version="0.0.1",
+    author="whiteblue3",
+    author_email="hd2dj07@gmail.com",
+    description="Private DJango Utility Functions",
+    long_description="Private DJango Utility Functions",
+    long_description_content_type="text/markdown",
+    url="https://github.com/whiteblue3/django_utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
