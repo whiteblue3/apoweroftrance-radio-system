74ffb52bc196f97df8f1ec06d9bc8422115a99a6
MOBIUS <hd2dj07@gmail.com>
2020-04-01 18:07:25 +0900

    django-accounts 이메일 부분 코드 수정

--- django-accounts/README.md
+++ django-accounts/README.md
@@ -1,3 +1,158 @@
 # How to packaging
-python3 setup.py sdist bdist_wheel
+    python3 setup.py sdist bdist_wheel
 
+# Install
+    pip3 install django-accounts
+    
+or
+
+    pip3 install accounts-0.0.1.tar.gz
+
+
+# Setup
+in settings.py:
+
+    INSTALLED_APPS = [
+        ...
+        
+        'accounts',
+        
+        'rest_framework',
+        'drf_yasg',
+            
+        ...
+    ]
+
+also configure with below
+
+    MIDDLEWARE = [
+    ...
+    
+        'drf_yasg.middleware.SwaggerExceptionMiddleware',
+    
+    ...
+    ]
+    
+    
+    AUTH_USER_MODEL = 'accounts.User'
+    
+    REST_FRAMEWORK = {
+        'DEFAULT_PERMISSION_CLASSES': (
+            'rest_framework.permissions.IsAuthenticated',
+        ),
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'accounts.backends.JWTAuthentication',
+        ),
+    }
+    
+    AUTHENTICATION_BACKENDS = (
+        'django.contrib.auth.backends.ModelBackend',
+        'accounts.backends.JWTAuthentication',
+    )
+    
+    
+    SWAGGER_SETTINGS = {
+        'SECURITY_DEFINITIONS': {
+            'Bearer': {
+                'type': 'apiKey',
+                'name': 'Authorization',
+                'in': 'header'
+            },
+        },
+    }
+    
+    
+    AES_KEY = '...'
+    AES_SECRET = '...'
+    
+    
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_HOST_USER = "hd2dj07@gmail.com"
+    EMAIL_HOST_PASSWORD = '"!Triace07"'
+    EMAIL_USE_TLS = True
+    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+    
+    
+    ACCOUNT_AUTHENTICATION_METHOD = 'email'
+    # ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS =1
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_UNIQUE_EMAIL = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    # ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+    # ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 5
+    # ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 86400 # 1 day in seconds
+    # ACCOUNT_LOGOUT_REDIRECT_URL = '/'
+    
+    DOMAIN_URL = "127.0.0.1:8080"
+    ACCOUNT_API_PATH = "/v1/user"
+    
+    RESET_PASSWORD_EMAIL_TITLE = "비밀번호 재설정 안내"
+    RESET_PASSWORD_EMAIL_BODY = "임시 비밀번호: %s\n" \
+                                "비밀번호 재설정하기: %s\n"
+    RESET_PASSWORD_EMAIL_HTML = "<!DOCTYPE html><html lang='kr'><body>" \
+                                "임시 비밀번호: %s<br />" \
+                                "<a href='%s' target='_blank'>비밀번호 재설정하기</a><br />" \
+                                "</body></html>"
+    
+    NOTIFY_SECURITY_ALERT_EMAIL_TITLE = "신규 로그인 알림"
+    NOTIFY_SECURITY_ALERT_EMAIL_BODY = "새로운 디바이스에서 계정에 로그인되었습니다. 본인이 로그인한 것이 맞나요?\n" \
+                                       "본인이 맞는 경우,\n" \
+                                       "이 메시지를 무시하셔도 됩니다. 별도로 취해야 할 조치는 없습니다.\n\n" \
+                                       "본인이 아닌 경우,\n" \
+                                       "계정이 해킹되었을 수 있으며, 계정 보안을 위해 몇 가지 조치를 취해야 합니다. \n" \
+                                       "조치를 취해주세요 -> %s\n" \
+                                       "보다 안전한 조치를 위해 빠르게 임시비밀번호로 변경하시는 것이 좋습니다\n" \
+                                       "비밀번호 변경: %s\n" \
+                                       "위의 링크를 클릭하여 임시비밀번호로 변경하신 후에는 반드시 비밀번호를 원하는 비밀번호로 변경해주세요\n"
+    NOTIFY_SECURITY_ALERT_EMAIL_HTML = "<!DOCTYPE html><html lang='kr'><body>" \
+                                       "새로운 디바이스에서 계정에 로그인되었습니다. 본인이 로그인한 것이 맞나요?<br />" \
+                                       "본인이 맞는 경우,<br />" \
+                                       "이 메시지를 무시하셔도 됩니다. 별도로 취해야 할 조치는 없습니다.<br /><br />" \
+                                       "본인이 아닌 경우,<br />" \
+                                       "계정이 해킹되었을 수 있으며, 계정 보안을 위해 몇 가지 조치를 취해야 합니다. " \
+                                       "시작하려면 지금 비밀번호를 재설정하세요.<br />" \
+                                       "<a href='%s' target='_blank'>조치를 취해주세요</a><br /><br />" \
+                                       "보다 안전한 조치를 위해 빠르게 임시비밀번호로 변경하시는 것이 좋습니다<br />" \
+                                       "<a href='%s' target='_blank'>비밀번호 변경</a><br />" \
+                                       "위의 링크를 클릭하여 임시비밀번호로 변경하신 후에는 반드시 비밀번호를 원하는 비밀번호로 변경해주세요<br />" \
+                                       "</body></html>"
+
+
+
+in urls.py:
+
+    from django.contrib import admin
+    from django.urls import path, re_path, include
+    from rest_framework import permissions
+    from drf_yasg.views import get_schema_view
+    from drf_yasg import openapi
+    
+    schema_url_patterns = [
+        ...
+        path('v1/user/', include('accounts.urls', namespace='user')),
+        ...
+    ]
+    
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="A Power of Trance Radio API",
+            default_version='v1',
+            description="사용법: 로그인 -> 리스폰스의 Token을 복사 -> 우상단의 Authorize클릭 -> Bearer <토큰> 입력후 Authorize클릭 -> 다른API 테스트"
+        ),
+        public=True,
+        permission_classes=(permissions.AllowAny,),
+        patterns=schema_url_patterns,
+    )
+    
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+    ] + schema_url_patterns
+    
+    urlpatterns += [
+        re_path(r'^docs(?P<format>\.json|\.yaml)/$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    ]
--- django-accounts/accounts/util.py
+++ django-accounts/accounts/util.py
@@ -14,6 +14,71 @@ from .error import ProfileDoesNotExist
 from django_utils import aes, storage
 
 
+if settings.DOMAIN_URL is None:
+    domain_url = "127.0.0.1:8080"
+else:
+    domain_url = settings.DOMAIN_URL
+
+if settings.ACCOUNT_API_PATH is None:
+    user_api_path = "/v1/user"
+else:
+    user_api_path = settings.ACCOUNT_API_PATH
+
+# Reset Password Email
+if settings.RESET_PASSWORD_EMAIL_TITLE is None:
+    reset_password_email_title = "비밀번호 재설정"
+else:
+    reset_password_email_title = settings.RESET_PASSWORD_EMAIL_TITLE
+
+if settings.RESET_PASSWORD_EMAIL_BODY is None:
+    reset_password_email_body = "임시 비밀번호: %s\n" \
+                                "비밀번호 재설정하기: %s\n"
+else:
+    reset_password_email_body = settings.RESET_PASSWORD_EMAIL_BODY
+
+if settings.RESET_PASSWORD_EMAIL_HTML is None:
+    reset_password_email_html = "<!DOCTYPE html><html lang='kr'><body>" \
+                                "임시 비밀번호: %s<br />" \
+                                "<a href='%s' target='_blank'>비밀번호 재설정하기</a><br />" \
+                                "</body></html>"
+else:
+    reset_password_email_html = settings.RESET_PASSWORD_EMAIL_HTML
+
+# Notify Security Alert Email
+if settings.NOTIFY_SECURITY_ALERT_EMAIL_TITLE is None:
+    notify_security_alert_email_title = "신규 로그인 알림"
+else:
+    notify_security_alert_email_title = settings.NOTIFY_SECURITY_ALERT_EMAIL_TITLE
+
+if settings.NOTIFY_SECURITY_ALERT_EMAIL_BODY is None:
+    notify_security_alert_email_body = "새로운 디바이스에서 계정에 로그인되었습니다. 본인이 로그인한 것이 맞나요?\n" \
+                                       "본인이 맞는 경우,\n" \
+                                       "이 메시지를 무시하셔도 됩니다. 별도로 취해야 할 조치는 없습니다.\n\n" \
+                                       "본인이 아닌 경우,\n" \
+                                       "계정이 해킹되었을 수 있으며, 계정 보안을 위해 몇 가지 조치를 취해야 합니다. \n" \
+                                       "조치를 취해주세요 -> %s\n" \
+                                       "보다 안전한 조치를 위해 빠르게 임시비밀번호로 변경하시는 것이 좋습니다\n" \
+                                       "비밀번호 변경: %s\n" \
+                                       "위의 링크를 클릭하여 임시비밀번호로 변경하신 후에는 반드시 비밀번호를 원하는 비밀번호로 변경해주세요\n"
+else:
+    notify_security_alert_email_body = settings.NOTIFY_SECURITY_ALERT_EMAIL_BODY
+
+if settings.NOTIFY_SECURITY_ALERT_EMAIL_HTML is None:
+    notify_security_alert_email_html = "<!DOCTYPE html><html lang='kr'><body>" \
+                                       "새로운 디바이스에서 계정에 로그인되었습니다. 본인이 로그인한 것이 맞나요?<br />" \
+                                       "본인이 맞는 경우,<br />" \
+                                       "이 메시지를 무시하셔도 됩니다. 별도로 취해야 할 조치는 없습니다.<br /><br />" \
+                                       "본인이 아닌 경우,<br />" \
+                                       "계정이 해킹되었을 수 있으며, 계정 보안을 위해 몇 가지 조치를 취해야 합니다. " \
+                                       "시작하려면 지금 비밀번호를 재설정하세요.<br />" \
+                                       "<a href='%s' target='_blank'>조치를 취해주세요</a><br /><br />" \
+                                       "보다 안전한 조치를 위해 빠르게 임시비밀번호로 변경하시는 것이 좋습니다<br />" \
+                                       "<a href='%s' target='_blank'>비밀번호 변경</a><br />" \
+                                       "</body></html>"
+else:
+    notify_security_alert_email_html = settings.NOTIFY_SECURITY_ALERT_EMAIL_HTML
+
+
 def accesslog(request, access_type, status, email, ip):
     data = {
         'email': email,
@@ -31,10 +96,6 @@ def accesslog(request, access_type, status, email, ip):
     accesslog_serializer = AccessLogSerializer(data=data)
     accesslog_serializer.is_valid(raise_exception=True)
     accesslog_serializer.save()
-
-
-def get_module_config():
-    return os.environ.get("DJANGO_SETTINGS")
 
 
 def get_profile(email):
@@ -92,54 +153,14 @@ def send_reset_password_email(email):
     json_encoded = json.dumps(confirm_data)
     encrypted_data = aes.aes_encrypt(json_encoded)
 
-    if "local" in get_module_config():
-        domain_url = "127.0.0.1:8000"
-    elif "dev" in get_module_config():
-        domain_url = "radio-dev.apoweroftrance.com"
-    elif "stage" in get_module_config():
-        domain_url = "radio-stage.apoweroftrance.com"
-    else:
-        domain_url = "radio.apoweroftrance.com"
-
-    base_url = "https://%s/v1/user/confirm_reset_password" % domain_url
+    base_url = "https://%s%s/confirm_reset_password" % (domain_url, user_api_path)
     confirm_link = "%s?auth_code=%s" % (base_url, urllib.parse.quote_plus(encrypted_data))
 
-    email_body = "회원님 안녕하세요.\n" \
-                 "\n" \
-                 "비밀번호 재설정을 요청하셨습니다.\n" \
-                 "임시 비밀번호는 %s 입니다\n" \
-                 "아래의 링크를 눌러 비밀번호를 재설정을 완료하세요.\n" \
-                 "\n" \
-                 "%s\n" \
-                 "\n" \
-                 "※ 회원님께서 위의 링크를 클릭하지 않으면 임시비밀번호는 최종 변경되지 않습니다.\n" \
-                 "※ 회원님께서 재설정 요청을 하지 않은 경우 아무런 조치를 취하실 필요가 없습니다.\n" \
-                 "※ 해당 메일은 답장 하실 수 없습니다. 궁금한 점이 있다면, 아래로 문의해 주시기 바랍니다.\n" \
-                 "- 문의 번호 : 010-6646-5931\n" \
-                 "- 문의 메일 : hd2dj07@gmail.com\n" \
-                 "\n" \
-                 "\n" \
-                 "감사합니다\n" % (new_password, confirm_link)
-
-    email_html = "<!DOCTYPE html><html lang='kr'><body>회원님 안녕하세요.<br />" \
-                 "<br />" \
-                 "비밀번호 재설정을 요청하셨습니다.<br />" \
-                 "임시 비밀번호는 %s 입니다<br />" \
-                 "아래의 링크를 눌러 비밀번호를 재설정하세요.<br />" \
-                 "<br />" \
-                 "<a href='%s' target='_blank'>비밀번호 재설정하기</a><br />" \
-                 "<br />" \
-                 "※ 회원님께서 위의 링크를 클릭하지 않으면 임시비밀번호는 최종 변경되지 않습니다.<br />" \
-                 "※ 회원님께서 재설정 요청을 하지 않은 경우 아무런 조치를 취하실 필요가 없습니다.<br />" \
-                 "※ 해당 메일은 답장 하실 수 없습니다. 궁금한 점이 있다면, 아래로 문의해 주시기 바랍니다.<br />" \
-                 "- 문의 번호 : 010-6646-55931<br />" \
-                 "- 문의 메일 : hd2dj07@gmail.com<br />" \
-                 "<br />" \
-                 "<br />" \
-                 "감사합니다</body></html>" % (new_password, confirm_link)
+    email_body = reset_password_email_body % (new_password, confirm_link)
+    email_html = reset_password_email_html % (new_password, confirm_link)
 
     from_email = settings.EMAIL_HOST_USER
-    send_mail("비밀번호 재설정 안내", message=email_body, html_message=email_html,
+    send_mail(reset_password_email_title, message=email_body, html_message=email_html,
               from_email=from_email, recipient_list=[email], fail_silently=True)
 
 
@@ -155,53 +176,15 @@ def notify_security_email(email, ip_address, token, request, accesslog_pk):
     json_encoded = json.dumps(confirm_data)
     encrypted_data = aes.aes_encrypt(json_encoded)
 
-    if "local" in get_module_config():
-        domain_url = "127.0.0.1:8000"
-    elif "dev" in get_module_config():
-        domain_url = "radio-dev.apoweroftrance.com"
-    elif "stage" in get_module_config():
-        domain_url = "radio-stage.apoweroftrance.com"
-    else:
-        domain_url = "radio.apoweroftrance.com"
-
-    base_url = "https://%s/v1/user/ban" % domain_url
+    base_url = "https://%s%s/ban" % (domain_url, user_api_path)
     ban_url = "%s?auth_code=%s" % (base_url, urllib.parse.quote_plus(encrypted_data))
 
-    base_url = "https://%s/v1/user/reset_password" % domain_url
+    base_url = "https://%s%s/reset_password" % (domain_url, user_api_path)
     reset_password_url = "%s/%s" % (base_url, urllib.parse.quote_plus(email))
 
-    email_body = "새로운 디바이스에서 계정에 로그인되었습니다. 본인이 로그인한 것이 맞나요?\n" \
-                 "*위치는 로그인 IP 주소를 기준으로 한 근접한 위치입니다.\n\n" \
-                 "본인이 맞는 경우,\n" \
-                 "이 메시지를 무시하셔도 됩니다. 별도로 취해야 할 조치는 없습니다.\n\n" \
-                 "본인이 아닌 경우,\n" \
-                 "계정이 해킹되었을 수 있으며, 계정 보안을 위해 몇 가지 조치를 취해야 합니다. \n" \
-                 "조치를 취해주세요 -> %s\n" \
-                 "보다 안전한 조치를 위해 빠르게 임시비밀번호로 변경하시는 것이 좋습니다\n" \
-                 "비밀번호 변경: %s\n" \
-                 "위의 링크를 클릭하여 임시비밀번호로 변경하신 후에는 반드시 비밀번호를 원하는 비밀번호로 변경해주세요\n" \
-                 "최근 6개월 이내에 사용한 이전의 비밀번호는 개인정보 보호법에 따라 재사용하실수 없습니다\n\n" \
-                 "GLO에서 보낸 이메일인지 어떻게 알 수 있나요?\n" \
-                 "본 이메일의 링크는 “https://”로 시작하고 “apoweroftrance.com”을 포함합니다.\n" \
-                 "브라우저에 표시된 자물쇠 아이콘을 통해서도 안전한 사이트인지 확인할 수 있습니다." \
-                 % (ban_url, reset_password_url)
-
-    email_html = "<!DOCTYPE html><html lang='kr'><body>새로운 디바이스에서 계정에 로그인되었습니다. 본인이 로그인한 것이 맞나요?<br />" \
-                 "*위치는 로그인 IP 주소를 기준으로 한 근접한 위치입니다.<br /><br />" \
-                 "본인이 맞는 경우,<br />" \
-                 "이 메시지를 무시하셔도 됩니다. 별도로 취해야 할 조치는 없습니다.<br /><br />" \
-                 "본인이 아닌 경우,<br />" \
-                 "계정이 해킹되었을 수 있으며, 계정 보안을 위해 몇 가지 조치를 취해야 합니다. 시작하려면 지금 비밀번호를 재설정하세요.<br />" \
-                 "<a href='%s' target='_blank'>조치를 취해주세요</a><br /><br />" \
-                 "보다 안전한 조치를 위해 빠르게 임시비밀번호로 변경하시는 것이 좋습니다<br />" \
-                 "<a href='%s' target='_blank'>비밀번호 변경</a><br />" \
-                 "위의 링크를 클릭하여 임시비밀번호로 변경하신 후에는 반드시 비밀번호를 원하는 비밀번호로 변경해주세요<br />" \
-                 "최근 6개월 이내에 사용한 이전의 비밀번호는 개인정보 보호법에 따라 재사용하실수 없습니다<br /><br />" \
-                 "GLO에서 보낸 이메일인지 어떻게 알 수 있나요?<br />" \
-                 "본 이메일의 링크는 “https://”로 시작하고 “apoweroftrance.com”을 포함합니다.<br />" \
-                 "브라우저에 표시된 자물쇠 아이콘을 통해서도 안전한 사이트인지 확인할 수 있습니다.</body></html>" \
-                 % (ban_url, reset_password_url)
+    email_body = notify_security_alert_email_body % (ban_url, reset_password_url)
+    email_html = notify_security_alert_email_html % (ban_url, reset_password_url)
 
     from_email = settings.EMAIL_HOST_USER
-    send_mail("신규 로그인 알림", message=email_body, html_message=email_html,
+    send_mail(notify_security_alert_email_title, message=email_body, html_message=email_html,
               from_email=from_email, recipient_list=[email], fail_silently=True)
