da63506913040c3386137b74fbe8abd7b1352336
MOBIUS <hd2dj07@gmail.com>
2020-04-12 01:21:40 +0900

    도커이미지 빌드후 실행시 오류 수정

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+set -e
 
+# Comment when production
 python3 -m pip install --no-cache-dir -r requirement.txt
-#
-#python3 manage.py collectstatic --noinput -i yes
+
+python3 manage.py collectstatic --noinput -i yes
 #python3 manage.py migrate --noinput
 #python3 manage.py makemigrations accounts
 #python3 manage.py makemigrations radio
-#python3 manage.py migrate
+python3 manage.py migrate
 
-uwsgi --ini /backend/uwsgi.ini
+uwsgi --show-config
