a1c5d97db327c4093c35c390ac374e4cfa2c939c
MOBIUS <hd2dj07@gmail.com>
2020-05-05 11:25:36 +0900

    modify shell script

--- icecast/build_push_docker_image.sh
+++ icecast/build_push_docker_image.sh
@@ -8,3 +8,4 @@ docker tag ${APP_NAME}:${VERSION} gcr.io/apoweroftrance/${APP_NAME}:${VERSION}
 docker tag ${APP_NAME}:${VERSION} gcr.io/apoweroftrance/${APP_NAME}:latest
 gcloud docker -- push gcr.io/apoweroftrance/${APP_NAME}:${VERSION}
 gcloud docker -- push gcr.io/apoweroftrance/${APP_NAME}:latest
+
