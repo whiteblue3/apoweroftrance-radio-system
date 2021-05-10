9f74dbb023bccf0d0286eb3e99c866e5de91aaf2
bwpark <bwpark@monstercube.kr>
2021-05-10 13:15:59 +0900

    fixed bugs: - cannot search tracks with keyword - cannot deploy to gcr.io

--- icecast/build_push_docker_image.sh
+++ icecast/build_push_docker_image.sh
@@ -6,6 +6,6 @@ VERSION=2.4.4
 docker build --no-cache=True -t ${APP_NAME}:${VERSION} .
 docker tag ${APP_NAME}:${VERSION} gcr.io/apoweroftrance/${APP_NAME}:${VERSION}
 docker tag ${APP_NAME}:${VERSION} gcr.io/apoweroftrance/${APP_NAME}:latest
-gcloud docker -- push gcr.io/apoweroftrance/${APP_NAME}:${VERSION}
-gcloud docker -- push gcr.io/apoweroftrance/${APP_NAME}:latest
+docker push gcr.io/apoweroftrance/${APP_NAME}:${VERSION}
+docker push gcr.io/apoweroftrance/${APP_NAME}:latest
 
--- nginx/build_push_docker_image.sh
+++ nginx/build_push_docker_image.sh
@@ -6,6 +6,6 @@ VERSION=1.17.10-r0
 docker build --no-cache=True -t ${APP_NAME}:${VERSION} .
 docker tag ${APP_NAME}:${VERSION} gcr.io/apoweroftrance/${APP_NAME}:${VERSION}
 docker tag ${APP_NAME}:${VERSION} gcr.io/apoweroftrance/${APP_NAME}:latest
-gcloud docker -- push gcr.io/apoweroftrance/${APP_NAME}:${VERSION}
-gcloud docker -- push gcr.io/apoweroftrance/${APP_NAME}:latest
+docker push gcr.io/apoweroftrance/${APP_NAME}:${VERSION}
+docker push gcr.io/apoweroftrance/${APP_NAME}:latest
 
